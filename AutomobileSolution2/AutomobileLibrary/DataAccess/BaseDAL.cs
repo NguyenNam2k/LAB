cf17c0f7f25de35d67168cb07116e8bbf2a4e0d8
NguyenNam2k <66913709+NguyenNam2k@users.noreply.github.com>
2024-02-18 04:03:45 +0700

    Add files via upload

---
+++ AutomobileSolution2/AutomobileLibrary/DataAccess/BaseDAL.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileLibrary.DataAccess
+{
+    public class BaseDAL
+    {
+        public StockDataProvider dataProvider { get; set; }
+        public SqlConnection connection=null;
+        public BaseDAL()
+        {
+            var connectionString=GetConnectionString();
+            dataProvider= new StockDataProvider(connectionString);
+        }
+        public string GetConnectionString()
+        {
+            string connectionString;
+            IConfiguration config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsetting.json", true, true).Build();
+            return connectionString = config["ConnectionString:MyStockDB"];
+        }
+        public void CloseConnection() => dataProvider.CloseConnection(connection);
+    }
+}
